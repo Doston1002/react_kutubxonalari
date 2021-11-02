66108d6c4cc951b5ac10e8c84f68462f5ad2c30a
Doston Po'latov <81279977+Doston1002@users.noreply.github.com>
2021-11-02 21:44:00 +0500

    Add files via upload

---
+++ react-shablon/react-practika/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sass/style.scss'
+import Home from './home/Home';
+function App() {
+  return (
+    <div className="app">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
